7f725ba964e0c05a41818187e206f9e0f0afab27
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2024-11-02 16:54:36 -0500

    working on home page

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,4 +1,4 @@
-import background from './assets/home_page.svg'
+import background from './assets/hospital.jpg'
 // import { useNavigate } from 'react-router-dom';
 
 function App() {
@@ -6,12 +6,19 @@ function App() {
   // const nav = useNavigate();
 
   return (
-    <div className='max-w-screen max-h-screen'>
-      {/* <div className='flex items-center justify-start h-full pl-12'> */}
-            <img className='h-full w-full' src={background}></img>
-      {/* </div> */}
+    <div className="h-screen flex items-center justify-center bg-[#1C2A4D]">
+      <div className="relative w-1/2 h-full" style={{ clipPath: 'ellipse(100% 100% at right)',  }}> {/* Adjust width for sliver effect */}
+        <img
+          src={background} // Replace with your image URL
+          alt="Peeking Image"
+          className="w-full h-full object-cover object-center" // Set to cover the container
+          // style={{ clipPath: 'ellipse(85% 100% at right)' }} // Clip the image to create the sliver effect
+        />
+        <div className="absolute inset-0 bg-[#FFB561] opacity-30 rounded-lg" />
+      </div>
+      <div></div>
     </div>
-  )
+  );
 }
 
 export default App
