8baf0627dc730af1b4f4e489f07d47f04383e1e4
jellybean-7 <angelicasharma@utexas.edu>
2024-11-02 18:06:09 -0500

    added login and tried adjusting home pic

--- frontend/src/SignIn.tsx
+++ frontend/src/SignIn.tsx
@@ -1,10 +1,20 @@
 import { useNavigate } from 'react-router-dom';
-
+import login_background from './assets/login_background.svg'
 function SignIn() {
   const nav = useNavigate();
 
   return (
-    <div>
+    <div className="relative h-screen w-screen">
+        <div className="relative flex items-center justify-end h-full">
+            {/* Main Content */}
+        <div className="w-full h-full">
+          <img
+            src={login_background}
+            alt="Main Background"
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
+        </div>
     </div>
   );
 }
