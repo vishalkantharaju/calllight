fbf72ee3d00102db99c9649359ff2baf58e00649
jellybean-7 <angelicasharma@utexas.edu>
2024-11-02 18:48:38 -0500

    login done

--- frontend/src/SignIn.tsx
+++ frontend/src/SignIn.tsx
@@ -1,7 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import login_background from './assets/login_background.svg'
+import { useState } from 'react'; 
+
 function SignIn() {
   const nav = useNavigate();
+  const [showForm, setShowForm] = useState(false); 
+  const [formSlide, setFormSlide] = useState(false);
+  const [nurseClicked, setNurseClicked] = useState(false);
+  const [patientClicked, setPatientClicked] = useState(false);
+
+  const toggleForm = (role: 'nurse' | 'patient') => {
+    setShowForm(true); 
+    setTimeout(() => {
+      setFormSlide(true); 
+    }, 10); 
+    if (role === 'nurse') {
+        setNurseClicked(true);
+      } else {
+        setPatientClicked(true);
+    }
+  };
 
   return (
     <div className="relative h-screen w-screen">
@@ -29,19 +47,52 @@ function SignIn() {
 
                 <div className="flex items-center space-x-4">
                     {/* Button */}
-                    <button className="mt-8 px-6 py-2 border-2 border-[#FFB561] 
-                        text-[#1C2A4D] font-bold bg-[#FFB561] rounded-md 
-                        hover:bg-[#FFB561] hover:text-white transition duration-200">
+                    <button onClick={() => toggleForm('nurse')} className={`mt-8 px-6 py-2 
+                        ${nurseClicked ? 'font-bold': 'font-semibold'} 
+                        bg-[#FFB561] text-[#1C2A4D] rounded-md transition duration-200`}>
                         Nurse
                     </button>
                     {/* Button */}
-                    <button className="mt-8 px-6 py-2 border-2 border-[#FFB561] 
-                        text-[#1C2A4D] font-bold bg-[#FFB561] rounded-md 
-                        hover:bg-[#FFB561] hover:text-white transition duration-200">
+                    <button onClick={() => toggleForm('patient')} className={`mt-8 px-6 py-2 
+                        ${patientClicked ? 'font-bold': 'font-semibold'} 
+                        bg-[#FFB561] text-[#1C2A4D] rounded-md transition duration-200`}>
                         Patient
                     </button>
                 </div>
             </div>
+
+            {/* Sliding Sign-In Form */}
+            {showForm && (
+        <div
+          className={`absolute top-36 right-36 w-1/3 h-full p-8 transition-transform duration-500 
+            ${formSlide ? 'translate-x-0' : 'translate-x-full'}`}>
+          <h2 className="text-2xl font-bold text-[#1C2A4D]">SIGN IN</h2>
+          <form className="mt-4">
+            <div>
+              <label className="block text-sm font-semibold text-[#1C2A4D]">Email</label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="border mt-2 rounded-md w-3/4 p-2"
+              />
+            </div>
+            <div className="mt-4">
+              <label className="block text-sm font-semibold text-[#1C2A4D]">Password</label>
+              <input
+                type="password"
+                placeholder="Enter your password"
+                className="border mt-2 rounded-md w-3/4 p-2"
+              />
+            </div>
+            <button
+              type="submit"
+              className="text-left mt-6 px-4 py-2 bg-[#FFB561] text-[#1C2A4D] font-bold rounded-md w-1/4 hover:text-white"
+            >
+              Sign In
+            </button>
+          </form>
+        </div>)}
+      
         </div>
     </div>
   );
