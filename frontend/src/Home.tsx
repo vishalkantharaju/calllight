4ccd3f2c928c20284274f0d8b18fcab1609c247c
jellybean-7 <angelicasharma@utexas.edu>
2024-11-02 17:22:39 -0500

    background homepage

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,21 +1,32 @@
-import background from './assets/front_page.png'
+import background from './assets/front_page.png';
+import background_overlay from './assets/background_overlay.svg';
 // import { useNavigate } from 'react-router-dom';
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // const nav = useNavigate();
-
   return (
-    <div className="h-screen flex items-center justify-end bg-[#1C2A4D]">
-      <div className="relative full w-1/3 sm:w-1/3 lg:w-1/2 h-full"> {/* Adjust width for sliver effect */}
-        <img
-          src={background} // Replace with your image URL
-          className="w-full h-full object-cover object-center" // Set to cover the container
-        />
+    <div className="relative h-screen w-screen bg-[#1C2A4D]">
+      <div className="relative flex items-center justify-end h-full">
+
+        {/* Main Content */}
+        <div className="w-1/3 sm:w-1/3 lg:w-1/2 h-full">
+          <img
+            src={background}
+            alt="Main Background"
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
+
+        {/* Transparent Overlay*/}
+        <div className="absolute inset-0">
+          <img
+            src={background_overlay}
+            className="w-full h-full object-cover opacity-30" 
+          />
+        </div>
+        
       </div>
-      <div></div>
     </div>
   );
 }
 
-export default App
+export default App;
